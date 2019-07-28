6c065d585033a5057f52f3cabc9c05daf3ea59c5
dependable-helper-6910 <ivana_pile@yahoo.com>
2019-07-28 14:04:09 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,5 +1,5 @@
-def levitation force = 0
-until levitation force == 10
+num_of_levitation_force = 6
+while num_of_levitation_force < 10
 puts "Wingardium Leviosa"
-levitation force += 1
+num_of_levitation_force += 1
 end
